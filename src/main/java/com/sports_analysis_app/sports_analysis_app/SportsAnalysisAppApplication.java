626168c26cfd83b769dd468487a72c8237249fd7
Anish Tilloo <anishtilloo@gmail.com>
2026-05-07 22:40:40 +0530

    intialised spring boot app

---
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/SportsAnalysisAppApplication.java
@@ -0,0 +1,13 @@
+package com.sports_analysis_app.sports_analysis_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsAnalysisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsAnalysisAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sports_analysis_app/sports_analysis_app/SportsAnalysisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sports_analysis_app.sports_analysis_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsAnalysisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
