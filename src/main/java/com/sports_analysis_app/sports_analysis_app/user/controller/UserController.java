1a4efd30acb2613c0d2b8e6007e462b83ab49a3d
Anish Tilloo <anishtilloo@gmail.com>
2026-05-09 17:54:14 +0530

    updated the controller and service with the updated logic of password and dtos and also hashing and token

--- src/main/java/com/sports_analysis_app/sports_analysis_app/user/controller/UserController.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/user/controller/UserController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sports_analysis_app.sports_analysis_app.user.entity.User;
+import com.sports_analysis_app.sports_analysis_app.user.dto.RegisterRequest;
+import com.sports_analysis_app.sports_analysis_app.user.dto.AuthResponse;
+import com.sports_analysis_app.sports_analysis_app.user.dto.LoginRequest;
 import com.sports_analysis_app.sports_analysis_app.user.service.UserService;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +20,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
     @Autowired
     private UserService service;
 
-    @PostMapping
-    public User postMethodName(@RequestBody User user) {
-        return service.createUser(user.getEmail(), user.getName());
+    @PostMapping("/register")
+    public AuthResponse register(@RequestBody RegisterRequest request) {
+        return service.registerUser(request.getEmail(), request.getName(), request.getPassword());
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest request) {
+        return service.loginUser(request.getEmail(), request.getPassword());
     }
 
     @GetMapping("/{id}")
@@ -30,7 +39,7 @@ public class UserController {
         return service.getUserById(id);
     }
 
-    @GetMapping("/{email}")
+    @GetMapping("/email/{email}")
     public User getUser(@PathVariable String email) {
         return service.getUserByEmail(email);
     }
--- src/main/java/com/sports_analysis_app/sports_analysis_app/user/service/UserService.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/user/service/UserService.java
@@ -3,6 +3,9 @@ package com.sports_analysis_app.sports_analysis_app.user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sports_analysis_app.sports_analysis_app.security.JwtUtil;
+import com.sports_analysis_app.sports_analysis_app.security.PasswordEncoder;
+import com.sports_analysis_app.sports_analysis_app.user.dto.AuthResponse;
 import com.sports_analysis_app.sports_analysis_app.user.entity.User;
 import com.sports_analysis_app.sports_analysis_app.user.repository.UserRepository;
 
@@ -11,7 +14,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User createUser (String email, String name) {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+    public AuthResponse registerUser (String email, String name, String password) {
         if (email == null || email.trim().isEmpty()) {
             throw new IllegalArgumentException("Email is Required");
         }
@@ -26,11 +36,42 @@ public class UserService {
             throw new IllegalArgumentException("Name is Required");
         }
 
-        User user = new User();
-        user.setEmail(email);
-        user.setName(name);
+        if (password == null || password.trim().isEmpty()) {
+            throw new IllegalArgumentException("Password is Required");
+        }
 
-        return userRepository.save(user);
+        long now = System.currentTimeMillis();
+        String hashedPassword = passwordEncoder.hashPassword(password);
+        User user = new User(name, email, hashedPassword, now, now);
+        User saveUser = userRepository.save(user);
+
+        String token = jwtUtil.generateToken(email, saveUser.getId());
+
+        return new AuthResponse(saveUser.getId(), token, saveUser.getEmail(), "User registered successfully");
+    }
+
+    public AuthResponse loginUser(String email, String password) {
+        if (email == null || email.trim().isEmpty()) {
+            throw new IllegalArgumentException("Email is Required");
+        }
+
+        if (password == null || password.trim().isEmpty()) {
+            throw new IllegalArgumentException("Password is Required");
+        }
+
+        User existingUser = userRepository.findByEmail(email);
+
+        if (existingUser == null) {
+            throw new IllegalArgumentException("Email already registered");
+        }
+
+        if (!passwordEncoder.verifyPassword(password, existingUser.getPassword())) {
+            throw new IllegalArgumentException("Invalid Email or Password");
+        }
+
+        String token = jwtUtil.generateToken(email, existingUser.getId());
+
+        return new AuthResponse(existingUser.getId(), token, existingUser.getEmail(), "Login Successful");
     }
 
     public User getUserById (Long id) {
