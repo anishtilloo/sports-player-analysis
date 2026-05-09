d5524078c8c21c5b619caad46c5c9afbe020a1f2
Anish Tilloo <anishtilloo@gmail.com>
2026-05-09 17:51:30 +0530

    moved hello world controller to a common package

--- src/main/java/com/sports_analysis_app/sports_analysis_app/common/controller/HelloWorldController.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/common/controller/HelloWorldController.java
@@ -1,10 +1,10 @@
-package com.sports_analysis_app.sports_analysis_app;
+package com.sports_analysis_app.sports_analysis_app.common.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorld {
+public class HelloWorldController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
@@ -15,4 +15,3 @@ public class HelloWorld {
         return "Hello this is my first spring boot api";
     }
 }
-
