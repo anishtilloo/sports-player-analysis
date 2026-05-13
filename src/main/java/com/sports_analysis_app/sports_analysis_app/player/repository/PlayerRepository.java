ca0428cea9a7561c23b2bd7fde1cb652015b1499
Anish Tilloo <anishtilloo@gmail.com>
2026-05-13 23:11:01 +0530

    added different methods to query from the DB

--- src/main/java/com/sports_analysis_app/sports_analysis_app/player/repository/PlayerRepository.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/player/repository/PlayerRepository.java
@@ -1,5 +1,7 @@
 package com.sports_analysis_app.sports_analysis_app.player.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,13 @@ import com.sports_analysis_app.sports_analysis_app.player.entity.Player;
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     Player findByEmail(String email);
+
+    Player findByNameContainingIgnoreCases(String name);
+
+    List<Player> findByRoleContainingIgnoreCases(String role);
+
+    List<Player> findPlayersByTeam(String team);
+
+    List<Player> findByEmailContainingIgnoreCasesOrNameContainingIgnoreCases(String email, String name);
         
 }
