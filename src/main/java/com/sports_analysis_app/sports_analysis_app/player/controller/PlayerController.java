ec84aea368244c72e928c0ea508c34b41a237f99
Anish Tilloo <anishtilloo@gmail.com>
2026-05-13 23:09:33 +0530

    added apis for searching finding by team and getting all players etc

--- src/main/java/com/sports_analysis_app/sports_analysis_app/player/controller/PlayerController.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/player/controller/PlayerController.java
@@ -1,13 +1,14 @@
 package com.sports_analysis_app.sports_analysis_app.player.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sports_analysis_app.sports_analysis_app.player.dto.PlayerRequest;
+import com.sports_analysis_app.sports_analysis_app.player.dto.PlayerUpdateRequest;
 import com.sports_analysis_app.sports_analysis_app.player.entity.Player;
 import com.sports_analysis_app.sports_analysis_app.player.service.PlayerService;
 
@@ -16,6 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 @RestController
@@ -43,5 +47,35 @@ public class PlayerController {
     @GetMapping("/email/{email}")
     public Player getUser(@PathVariable String email) {
         return service.getPlayerByEmail(email);
+    }
+    
+    @GetMapping("/name/{name}")
+    public Player getMethodName(@PathVariable String name) {
+        return service.getPlayerByName(name);
+    }
+    
+    @GetMapping
+    public List<Player> getAllPlayers() {
+        return service.getAllPlayers();
+    }
+
+    @GetMapping("/role/{role}")
+    public List<Player> getPlayersByRole(@PathVariable String role) {
+        return service.getPlayersByRole(role);
+    }
+    
+    @GetMapping("/team/{team}")
+    public List<Player> getPlayersByTeam(@PathVariable String team) {
+        return service.getPlayersByTeam(team);
+    }
+
+    @GetMapping("/search")
+    public List<Player> searchPlayers(@PathVariable String searchQuery) {
+        return service.searchPlayers(searchQuery);
+    }
+    
+    @PutMapping("/{id}")
+    public Player updatePlayer(@PathVariable Long id, @RequestBody PlayerUpdateRequest request) {
+        return service.updatePlayer(id, request);
     }
 }
