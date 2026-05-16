cf89d1241ce4e918e27b174154f0403ed9329aa4
Anish Tilloo <anishtilloo@gmail.com>
2026-05-16 17:20:18 +0530

    removed extra import

--- src/main/java/com/sports_analysis_app/sports_analysis_app/player/controller/PlayerController.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/player/controller/PlayerController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
