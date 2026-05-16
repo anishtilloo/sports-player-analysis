8eac57a807b7deeede72c0f8629d8b2223219411
Anish Tilloo <anishtilloo@gmail.com>
2026-05-16 17:15:26 +0530

    created an custom annotation to cleanly protect routes using authentication

---
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/annotation/auth/AuthRequired.java
@@ -0,0 +1,13 @@
+package com.sports_analysis_app.sports_analysis_app.annotation.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("isAuthenticated()")
+public @interface AuthRequired {}
