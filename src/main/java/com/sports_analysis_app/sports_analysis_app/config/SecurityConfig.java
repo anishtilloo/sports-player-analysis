a7b7434c038d4363ec263a3b448e2ceaa333142d
Anish Tilloo <anishtilloo@gmail.com>
2026-05-16 17:16:18 +0530

    updated the security config by addeding jwt filter

--- src/main/java/com/sports_analysis_app/sports_analysis_app/config/SecurityConfig.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/config/SecurityConfig.java
@@ -1,15 +1,32 @@
 package com.sports_analysis_app.sports_analysis_app.config;
 
+import com.sports_analysis_app.sports_analysis_app.security.JwtAuthenticationFilter;
+import com.sports_analysis_app.sports_analysis_app.security.JwtAuthenticationEntryPoint;
+import com.sports_analysis_app.sports_analysis_app.security.JwtAccessDeniedHandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -19,12 +36,20 @@ public class SecurityConfig {
             
             // 2. Configure Authorization
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/h2-console/**").permitAll()
+
+                .requestMatchers("/api/player/**").authenticated()
+                .requestMatchers("/api/users/**").authenticated()
                 .anyRequest().authenticated()
             )
+
+            .exceptionHandling(exception -> exception
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+            )
             
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             // 3. Handle H2 Console frames (Allows the UI to render)
             .headers(headers -> headers.frameOptions(frame -> frame.disable()))
             
