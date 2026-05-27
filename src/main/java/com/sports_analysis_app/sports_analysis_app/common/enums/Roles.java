2078f6ad76152bca64d0add718f67d4fd6afece0
Anish Tilloo <anishtilloo@gmail.com>
2026-05-27 22:38:59 +0530

    added proper roles

---
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/common/enums/Roles.java
@@ -0,0 +1,14 @@
+package com.sports_analysis_app.sports_analysis_app.common.enums;
+
+public enum Roles {
+    USER,
+    PLAYER,
+    ANALYST,
+    ORG_OWNER,
+    ORG_ADMIN,
+    ORG_COACH,
+    ORG_SCOUT,
+    ORG_MEDICAL_STAFF,
+    ORG_NUTRITIONIST,
+    ORG_SUPPORT_STAFF,
+}
