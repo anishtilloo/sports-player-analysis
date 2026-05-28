a666f71647289e0e1128a1e2d449eddfcca4ffa2
Anish Tilloo <anishtilloo@gmail.com>
2026-05-28 23:20:15 +0530

    removed the auth methods from user service

--- src/main/java/com/sports_analysis_app/sports_analysis_app/user/service/UserService.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/user/service/UserService.java
@@ -3,9 +3,6 @@ package com.sports_analysis_app.sports_analysis_app.user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sports_analysis_app.sports_analysis_app.security.JwtUtil;
-import com.sports_analysis_app.sports_analysis_app.security.PasswordEncoder;
-import com.sports_analysis_app.sports_analysis_app.user.dto.AuthResponse;
 import com.sports_analysis_app.sports_analysis_app.user.entity.User;
 import com.sports_analysis_app.sports_analysis_app.user.repository.UserRepository;
 
@@ -14,91 +11,8 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private JwtUtil jwtUtil;
-
-
-    public AuthResponse registerUser (String email, String name, String password) {
-        if (email == null || email.trim().isEmpty()) {
-            throw new IllegalArgumentException("Email is Required");
-        }
-
-        User existingUser = userRepository.findByEmail(email);
-
-        if (existingUser != null) {
-            throw new IllegalArgumentException("Email already registered");
-        }
-
-        if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("Name is Required");
-        }
-
-        if (password == null || password.trim().isEmpty()) {
-            throw new IllegalArgumentException("Password is Required");
-        }
-
-        long now = System.currentTimeMillis();
-        String hashedPassword = passwordEncoder.hashPassword(password);
-        User user = new User(name, email, hashedPassword, now, now);
-        User saveUser = userRepository.save(user);
-
-        String accessToken = jwtUtil.generateAccessToken(email, saveUser.getId());
-        String refreshToken = jwtUtil.generateRefreshToken(email, saveUser.getId());
-
-        return new AuthResponse(saveUser.getId(), accessToken, refreshToken, saveUser.getEmail(), "User registered successfully");
-    }
-
-    public AuthResponse loginUser(String email, String password) {
-        if (email == null || email.trim().isEmpty()) {
-            throw new IllegalArgumentException("Email is Required");
-        }
-
-        if (password == null || password.trim().isEmpty()) {
-            throw new IllegalArgumentException("Password is Required");
-        }
-
-        User existingUser = userRepository.findByEmail(email);
-
-        if (existingUser == null) {
-            throw new IllegalArgumentException("Email already registered");
-        }
-
-        if (!passwordEncoder.verifyPassword(password, existingUser.getPassword())) {
-            throw new IllegalArgumentException("Invalid Email or Password");
-        }
-
-        String accessToken = jwtUtil.generateAccessToken(email, existingUser.getId());
-        String refreshToken = jwtUtil.generateRefreshToken(email, existingUser.getId());
-
-        return new AuthResponse(existingUser.getId(), accessToken,  refreshToken, existingUser.getEmail(), "Login Successful");
-    }
-
-    public AuthResponse refreshUserSession(String refreshToken) {
-        if (refreshToken == null || refreshToken.trim().isEmpty()) {
-            throw new IllegalArgumentException("Refresh Token is Required");
-        }
-
-        boolean isRefreshTokenVerified = jwtUtil.validateRefreshToken(refreshToken);
-
-        if (!isRefreshTokenVerified) {
-            throw new IllegalArgumentException("Refresh Token not verified");
-        }
-
-        String email = jwtUtil.extractEmailFromRefreshToken(refreshToken);
-        Long userId = jwtUtil.extractUserIdFromRefreshToken(refreshToken);
-
-        String accessToken = jwtUtil.generateAccessToken(email, userId);
-        String newRefreshToken = jwtUtil.generateRefreshToken(email, userId);
-
-        return new AuthResponse(userId, accessToken,  newRefreshToken, email, "User Session Refresh Successful");
-
-    }
-
-    public User getUserById (Long id) {
-        return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("User not found"));
+    public User getUserByUid (String uid) {
+        return userRepository.findByUid(uid);
     }
 
     public User getUserByEmail (String email) {
