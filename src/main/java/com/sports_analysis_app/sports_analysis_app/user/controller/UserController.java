d32f34040f69f093c79a1c0c94b10deac340fa15
Anish Tilloo <anishtilloo@gmail.com>
2026-05-28 23:23:06 +0530

    removed the auth routes from user controller

--- src/main/java/com/sports_analysis_app/sports_analysis_app/user/controller/UserController.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/user/controller/UserController.java
@@ -5,48 +5,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sports_analysis_app.sports_analysis_app.user.entity.User;
-import com.sports_analysis_app.sports_analysis_app.user.dto.RegisterRequest;
-import com.sports_analysis_app.sports_analysis_app.user.dto.AuthResponse;
-import com.sports_analysis_app.sports_analysis_app.user.dto.LoginRequest;
-import com.sports_analysis_app.sports_analysis_app.user.dto.RefreshRequest;
 import com.sports_analysis_app.sports_analysis_app.user.service.UserService;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
-    private UserService service;
+    private UserService userService;
 
-    @PostMapping("/register")
-    public AuthResponse register(@RequestBody RegisterRequest request) {
-        return service.registerUser(request.getEmail(), request.getName(), request.getPassword());
-    }
-
-    @PostMapping("/login")
-    public AuthResponse login(@RequestBody LoginRequest request) {
-        return service.loginUser(request.getEmail(), request.getPassword());
-    }
-
-    @PostMapping("/refresh")
-    public AuthResponse login(@RequestBody RefreshRequest request) {
-        return service.refreshUserSession(request.getRefreshToken());
-    }
-
-    @GetMapping("/me/{id}")
-    public User getUser(@PathVariable Long id) {
-        return service.getUserById(id);
+    @GetMapping("/me/{uid}")
+    public User getUser(@PathVariable String uid) {
+        return userService.getUserById(uid);
     }
 
     @GetMapping("/email/{email}")
-    public User getUser(@PathVariable String email) {
-        return service.getUserByEmail(email);
+    public User getUserByEmail(@PathVariable String email) {
+        return userService.getUserByEmail(email);
     }
 }
