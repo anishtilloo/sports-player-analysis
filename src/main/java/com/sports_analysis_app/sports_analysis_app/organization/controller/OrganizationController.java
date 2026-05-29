f6c1553d26c9639e575b78eda54a04de3d1c48a1
Anish Tilloo <anishtilloo@gmail.com>
2026-05-30 03:17:09 +0530

    added org controller

--- src/main/java/com/sports_analysis_app/sports_analysis_app/organization/controller/OrganizationController.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/organization/controller/OrganizationController.java
@@ -1,8 +1,23 @@
 package com.sports_analysis_app.sports_analysis_app.organization.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.sports_analysis_app.sports_analysis_app.annotation.auth.AuthRequired;
+import com.sports_analysis_app.sports_analysis_app.organization.service.OrganizationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class OrganizationController {
     
+    @Autowired
+    private OrganizationService service;
+
+    @AuthRequired
+    @GetMapping("api/organization/{uid}")
+    public String getOrganizationByUid(@RequestParam String uid) {
+        return service.getOrganizationByUid(uid).toString();
+    }
 }
