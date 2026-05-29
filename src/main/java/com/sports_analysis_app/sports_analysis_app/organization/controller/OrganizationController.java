b308c551c081b9734c099b23f2257858d6a90d19
Anish Tilloo <anishtilloo@gmail.com>
2026-05-30 02:13:22 +0530

    added organization controller

---
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/organization/controller/OrganizationController.java
@@ -0,0 +1,8 @@
+package com.sports_analysis_app.sports_analysis_app.organization.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrganizationController {
+    
+}
