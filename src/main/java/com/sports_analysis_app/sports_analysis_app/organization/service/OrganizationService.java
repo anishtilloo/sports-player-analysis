18f35f6627f4f32594100ed2ba7c0900064626ff
Anish Tilloo <anishtilloo@gmail.com>
2026-05-30 23:39:45 +0530

    added service annotation for org service

--- src/main/java/com/sports_analysis_app/sports_analysis_app/organization/service/OrganizationService.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/organization/service/OrganizationService.java
@@ -1,10 +1,12 @@
 package com.sports_analysis_app.sports_analysis_app.organization.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.sports_analysis_app.sports_analysis_app.organization.entity.Organization;
 import com.sports_analysis_app.sports_analysis_app.organization.repository.OrganizationRepository;
 
+@Service
 public class OrganizationService {
     
     @Autowired
