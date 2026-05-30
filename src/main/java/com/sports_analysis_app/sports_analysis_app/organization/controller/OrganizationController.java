831d15aad5f5cb608a07a3af8f908cc65d20e7d9
Anish Tilloo <anishtilloo@gmail.com>
2026-05-30 23:39:05 +0530

    added commmon request mapping for org controller

--- src/main/java/com/sports_analysis_app/sports_analysis_app/organization/controller/OrganizationController.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/organization/controller/OrganizationController.java
@@ -6,17 +6,19 @@ import org.springframework.stereotype.Controller;
 import com.sports_analysis_app.sports_analysis_app.annotation.auth.AuthRequired;
 import com.sports_analysis_app.sports_analysis_app.organization.service.OrganizationService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
+@RequestMapping("/api/organization")
 public class OrganizationController {
     
     @Autowired
     private OrganizationService service;
 
     @AuthRequired
-    @GetMapping("api/organization/{uid}")
+    @GetMapping("/{uid}")
     public String getOrganizationByUid(@RequestParam String uid) {
         return service.getOrganizationByUid(uid).toString();
     }
