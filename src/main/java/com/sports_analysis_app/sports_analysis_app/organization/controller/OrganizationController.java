4ce4a3b214da1ccbc1da2b73cfe0cf05896bb37d
Anish Tilloo <anishtilloo@gmail.com>
2026-06-02 23:26:51 +0530

    added controller for creating new org

--- src/main/java/com/sports_analysis_app/sports_analysis_app/organization/controller/OrganizationController.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/organization/controller/OrganizationController.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.sports_analysis_app.sports_analysis_app.annotation.auth.AuthRequired;
+import com.sports_analysis_app.sports_analysis_app.organization.dto.CreateOrganizationRequestDto;
+import com.sports_analysis_app.sports_analysis_app.organization.dto.CreateOrganizationResponse;
 import com.sports_analysis_app.sports_analysis_app.organization.service.OrganizationService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -22,4 +27,11 @@ public class OrganizationController {
     public String getOrganizationByUid(@RequestParam String uid) {
         return service.getOrganizationByUid(uid).toString();
     }
+
+    @AuthRequired
+    @PostMapping("")
+    public CreateOrganizationResponse postMethodName(@RequestBody CreateOrganizationRequestDto entity) {
+        return service.createOrganization(entity);
+    }
+    
 }
