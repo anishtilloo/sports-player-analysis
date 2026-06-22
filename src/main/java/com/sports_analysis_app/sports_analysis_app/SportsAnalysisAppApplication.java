133ffcf731a5a4c1a33bf20f5344e2b7ff21a060
Anish Tilloo <anishtilloo@gmail.com>
2026-06-22 21:35:04 +0530

    build: add validation dependency and enable JPA auditing
    Added spring-boot-starter-validation so @Valid works in controllers.
    Added @EnableJpaAuditing so @CreatedDate/@LastModifiedDate populate.
    Removed non-standard test dependencies; use spring-boot-starter-test.

--- src/main/java/com/sports_analysis_app/sports_analysis_app/SportsAnalysisAppApplication.java
+++ src/main/java/com/sports_analysis_app/sports_analysis_app/SportsAnalysisAppApplication.java
@@ -2,8 +2,10 @@ package com.sports_analysis_app.sports_analysis_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SportsAnalysisAppApplication {
 
 	public static void main(String[] args) {
